cedb8704a3e94f48420cb5af6ec746c53e03487f
YoungjinKim <talkingjuly@gmail.com>
2024-12-27 11:52:14 +0900

    second commit

---
+++ src/main/java/com/docteory07/mweireiteng/MweireitengApplication.java
@@ -0,0 +1,13 @@
+package com.docteory07.mweireiteng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MweireitengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MweireitengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docteory07/mweireiteng/MweireitengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docteory07.mweireiteng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MweireitengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
